e6145699e0cd1d3f2e23632cdec48d6694637017
Wildan Aizzaddin <39244724+aizzaddin@users.noreply.github.com>
2022-09-27 23:01:08 +0700

    Add API App

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Supermarket.apps.SupermarketConfig',
+    'Api.apps.ApiConfig',
+
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("Supermarket.urls"))
+    path("", include("Supermarket.urls")),
+    path("api/", include("Api.urls"))
 ]
