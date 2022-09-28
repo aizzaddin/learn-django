9413f8dbeca7ab9ba32254d9aafe9e737f00673f
Wildan Aizzaddin <39244724+aizzaddin@users.noreply.github.com>
2022-09-28 21:32:15 +0700

    :hammer: Add Get Detail Product using RetrieveAPIView

--- djangoProject/Api/urls.py
+++ djangoProject/Api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProductApiView
+from .views import ProductApiView, ProductApiDetailView
 
 
 urlpatterns = [
-    path("product", ProductApiView.as_view(), name="book_list")
+    path("product", ProductApiView.as_view(), name="product_list"),
+    path("product/<str:pk>", ProductApiDetailView.as_view(), name="product_detail")
 ]
--- djangoProject/Api/views.py
+++ djangoProject/Api/views.py
@@ -7,3 +7,8 @@ from .serializers import ProductSerializer
 class ProductApiView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductApiDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
